133224321bfa204e7e3f3ebe1d377f42d073a51d
Jozuo <jozuo.dev@gmail.com>
2021-07-15 09:41:57 +0900

    fix(*): indexページ差し替え

--- html/index.php
+++ html/index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Index Page" />
     <link rel="icon" type="image/svg+xml" href="../../favicon.svg" />
     <link rel="stylesheet" href="../../reset.min.css" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="style.scss" />
     <title>index page</title>
-  </head>
-  <body>
-    Index Page
-  </body>
+</head>
+
+<body>
+    <?php phpinfo() ?>
+</body>
+
 </html>
